71f9f81e6f96b01497c87aa8dbe05878243f7056
Night <jfrancois.lai@gmail.com>
2015-03-12 23:20:40 +0100

    Add User and Quote models

---
+++ Gogocar/Dto/Model/Quote.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Gogocar\Dto\Model;
+
+use JMS\Serializer\Annotation as Serializer;
+
+class Quote
+{
+    /**
+     * @Serializer\Expose()
+     */
+    protected $id;
+
+    /**
+     * @Serializer\Expose()
+     *
+     * @var string
+     */
+    protected $details;
+
+    /**
+     * @Serializer\Expose()
+     *
+     * @var string
+     */
+    protected $price;
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param integer $id
+     *
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDetails()
+    {
+        return $this->details;
+    }
+
+    /**
+     * @param mixed $details
+     * @return $this
+     */
+    public function setDetails($details)
+    {
+        $this->details = $details;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     *
+     * @return $this
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
---
+++ Gogocar/Dto/Model/User.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Gogocar\Dto\Model;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use JMS\Serializer\Annotation as Serializer;
+
+class User
+{
+    protected $id;
+
+    /**
+     * @Serializer\Expose()
+     *
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * @Serializer\Exclude()
+     *
+     * @var string
+     */
+    protected $usernameCanonical;
+
+    /**
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @var string
+     */
+    protected $emailCanonical;
+
+    /**
+     * @var boolean
+     */
+    protected $enabled;
+
+    /**
+     * The salt to use for hashing
+     *
+     * @var string
+     */
+    protected $salt;
+
+    /**
+     * Encrypted password. Must be persisted.
+     *
+     * @var string
+     * @Serializer\Exclude();
+     */
+    protected $password;
+
+    /**
+     * Plain password. Used for model validation. Must not be persisted.
+     *
+     * @var string
+     */
+    protected $plainPassword;
+
+    /**
+     * @var \DateTime
+     */
+    protected $lastLogin;
+
+    /**
+     * Random string sent to the user email address in order to verify it
+     *
+     * @var string
+     */
+    protected $confirmationToken;
+
+    /**
+     * @var \DateTime
+     */
+    protected $passwordRequestedAt;
+
+    /**
+     * @var Collection
+     */
+    protected $groups;
+
+    /**
+     * @var boolean
+     */
+    protected $locked;
+
+    /**
+     * @var boolean
+     */
+    protected $expired;
+
+    /**
+     * @var \DateTime
+     */
+    protected $expiresAt;
+
+    /**
+     * @var array
+     */
+    protected $roles;
+
+    /**
+     * @var boolean
+     */
+    protected $credentialsExpired;
+
+    /**
+     * @var \DateTime
+     */
+    protected $credentialsExpireAt;
+}
